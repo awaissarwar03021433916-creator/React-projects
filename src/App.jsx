cfd646ac556262e3762f97b6523b17711dccc787
awais sarwar <awaissarwar03021433916@gmail.com>
2025-09-25 02:27:57 -0700

    fix: resolve background color not updating issue by switching from inline styles to dynamic Tailwind classes

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [color,setColor] = useState("bg-olive-600")
+return (
+   <div 
+    className="w-full h-screen "
+    style={{backgroundColor: color}}
+  >
+       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div  className='flex flex-wrap justify-center gap-3 shadow-2xl bg-white px-5 py-4 rounded-xl text-amber-50 '>
+          <button
+          onClick={()=>setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"red"}}
+          >RED</button>
+
+          <button
+          onClick={()=>setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"yellow"}}
+          
+          >YELLOW</button>
+          <button
+          onClick={()=>setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"blue"}}
+          
+          >BLUE</button>
+          <button
+          onClick={()=>setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"green"}}
+          
+          >GREEN</button>
+          <button
+          onClick={()=>setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"orange"}}
+          
+          >ORANGE</button>
+        </div>
+       </div>
+  </div>
+   
+  )
+}
+
+export default App
